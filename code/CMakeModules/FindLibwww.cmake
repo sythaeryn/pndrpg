17bb540ba8a866e635f172ce7810d25de17edb99
sfb <sfb@localhost>
2010-09-27 21:13:13 -0500

    Changed: Install Qt DLLs from bin instead of lib

---
+++ code/CMakeModules/FindLibwww.cmake
@@ -0,0 +1,133 @@
+#
+# Find the W3C libwww includes and library
+#
+# This module defines
+# LIBWWW_INCLUDE_DIR, where to find tiff.h, etc.
+# LIBWWW_LIBRARY, where to find the LibWWW library.
+# LIBWWW_FOUND, If false, do not try to use LibWWW.
+
+# also defined, but not for general use are
+IF(LIBWWW_LIBRARY AND LIBWWW_INCLUDE_DIR)
+  # in cache already
+  SET(LIBWWW_FIND_QUIETLY TRUE)
+ENDIF(LIBWWW_LIBRARY AND LIBWWW_INCLUDE_DIR)
+
+FIND_PATH(LIBWWW_INCLUDE_DIR
+  WWWInit.h
+  PATHS
+  /usr/local/include
+  /usr/include
+  /sw/include
+  /opt/local/include
+  /opt/csw/include
+  /opt/include
+  PATH_SUFFIXES libwww w3c-libwww
+)
+
+# when installing libwww on mac os x using macports the file wwwconf.h resides
+# in /opt/local/include and not in the real libwww include dir :/
+FIND_PATH(LIBWWW_ADDITIONAL_INCLUDE_DIR
+  wwwconf.h
+  PATHS
+  /usr/local/include
+  /usr/include
+  /sw/include
+  /opt/local/include
+  /opt/csw/include
+  /opt/include
+)
+
+# combine both include directories into one variable
+IF(LIBWWW_ADDITIONAL_INCLUDE_DIR)
+  SET(LIBWWW_INCLUDE_DIR ${LIBWWW_INCLUDE_DIR} ${LIBWWW_ADDITIONAL_INCLUDE_DIR})
+ENDIF(LIBWWW_ADDITIONAL_INCLUDE_DIR)
+
+# helper to find all the libwww sub libraries
+MACRO(FIND_WWW_LIBRARY MYLIBRARY)
+  FIND_LIBRARY(${MYLIBRARY}
+    NAMES ${ARGN}
+    PATHS
+    /usr/local/lib
+    /usr/lib
+    /usr/local/X11R6/lib
+    /usr/X11R6/lib
+    /sw/lib
+    /opt/local/lib
+    /opt/csw/lib
+    /opt/lib
+    /usr/freeware/lib64
+  )
+  
+  IF(${MYLIBRARY})
+    SET(LIBWWW_LIBRARIES ${LIBWWW_LIBRARIES} ${${MYLIBRARY}})
+  ELSE(${MYLIBRARY})
+    MESSAGE(STATUS "Warning: Libwww: Library not found: ${MYLIBRARY}")
+  ENDIF(${MYLIBRARY})
+  
+ENDMACRO(FIND_WWW_LIBRARY MYLIBRARY)
+
+# on eg. mac os x and arch linux, libwww sub libraries are not "inter-linked"
+# we need to link them all manually
+
+FIND_WWW_LIBRARY(LIBWWWAPP_LIBRARY wwwapp)
+FIND_WWW_LIBRARY(LIBWWWCORE_LIBRARY wwwcore)
+FIND_WWW_LIBRARY(LIBWWWDIR_LIBRARY wwwdir)
+FIND_WWW_LIBRARY(LIBWWWFILE_LIBRARY wwwfile)
+FIND_WWW_LIBRARY(LIBWWWFTP_LIBRARY wwwftp)
+FIND_WWW_LIBRARY(LIBWWWGOPHER_LIBRARY wwwgopher)
+FIND_WWW_LIBRARY(LIBWWWHTML_LIBRARY wwwhtml)
+FIND_WWW_LIBRARY(LIBWWWHTTP_LIBRARY wwwhttp)
+FIND_WWW_LIBRARY(LIBWWWINIT_LIBRARY wwwinit)
+FIND_WWW_LIBRARY(LIBWWWMIME_LIBRARY wwwmime)
+FIND_WWW_LIBRARY(LIBWWWCACHE_LIBRARY wwwcache)
+FIND_WWW_LIBRARY(LIBWWWMUX_LIBRARY wwwmux)
+FIND_WWW_LIBRARY(LIBWWWNEWS_LIBRARY wwwnews)
+FIND_WWW_LIBRARY(LIBWWWSTREAM_LIBRARY wwwstream)
+FIND_WWW_LIBRARY(LIBWWWTELNET_LIBRARY wwwtelnet)
+FIND_WWW_LIBRARY(LIBWWWTRANS_LIBRARY wwwtrans)
+FIND_WWW_LIBRARY(LIBWWWUTILS_LIBRARY wwwutils)
+FIND_WWW_LIBRARY(LIBWWWXML_LIBRARY wwwxml)
+FIND_WWW_LIBRARY(LIBWWWZIP_LIBRARY wwwzip)
+FIND_WWW_LIBRARY(LIBMD5_LIBRARY md5)
+
+# Windows libwww version needs GNU Regex
+IF(WIN32 AND WITH_STATIC)
+  FIND_PATH(LIBWWW_REGEX_INCLUDE_DIR
+    regex.h
+    PATHS
+    /usr/local/include
+    /usr/include
+    /sw/include
+    /opt/local/include
+    /opt/csw/include
+    /opt/include
+  )
+
+  # combine both include directories into one variable
+  IF(LIBWWW_REGEX_INCLUDE_DIR)
+    SET(LIBWWW_INCLUDE_DIR ${LIBWWW_INCLUDE_DIR} ${LIBWWW_REGEX_INCLUDE_DIR})
+    FIND_WWW_LIBRARY(LIBREGEX_LIBRARY gnu_regex)
+  ENDIF(LIBWWW_REGEX_INCLUDE_DIR)
+ENDIF(WIN32 AND WITH_STATIC)
+
+IF(WITH_STATIC)
+  # If compiled with expat support
+  FIND_PACKAGE(EXPAT QUIET)
+  IF(EXPAT_FOUND)
+    SET(LIBWWW_INCLUDE_DIR ${LIBWWW_INCLUDE_DIR} ${EXPAT_INCLUDE_DIRS})
+    SET(LIBWWW_LIBRARIES ${LIBWWW_LIBRARIES} ${EXPAT_LIBRARIES})
+  ENDIF(EXPAT_FOUND)
+
+  # If compiled with OpenSSL support
+  FIND_PACKAGE(OpenSSL QUIET)
+  IF(OPENSSL_FOUND)
+    SET(LIBWWW_INCLUDE_DIR ${LIBWWW_INCLUDE_DIR} ${OPENSSL_INCLUDE_DIR})
+    SET(LIBWWW_LIBRARIES ${LIBWWW_LIBRARIES} ${OPENSSL_LIBRARIES})
+  ENDIF(OPENSSL_FOUND)
+ENDIF(WITH_STATIC)
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(LibWWW DEFAULT_MSG
+  LIBWWW_LIBRARIES
+  LIBWWW_INCLUDE_DIR
+)
---
+++ code/nel/src/3d/driver/opengl/unix_event_emitter.h
@@ -0,0 +1,109 @@
+// NeL - MMORPG Framework <http://dev.ryzom.com/projects/nel/>
+// Copyright (C) 2010  Winch Gate Property Limited
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef NL_UNIX_EVENT_EMITTER_H
+#define NL_UNIX_EVENT_EMITTER_H
+
+#include "nel/misc/types_nl.h"
+#include "nel/misc/event_emitter.h"
+#include "nel/misc/events.h"
+#include "nel/misc/game_device_events.h"
+
+#if defined(NL_OS_UNIX) && !defined(NL_OS_MAC)
+
+#include <GL/gl.h>
+#include <GL/glx.h>
+
+namespace NLMISC {
+
+
+/**
+ * CEventEmitter UNIX implementation
+ * \author Vianney Lecroart
+ * \author Nevrax France
+ * \date 2000
+ */
+class CUnixEventEmitter : public IEventEmitter
+{
+public:
+
+	/// Constructor
+	CUnixEventEmitter();
+	virtual ~CUnixEventEmitter();
+
+	/**
+	 * initialize CUnixEventEmitter
+	 */
+	void init(Display *dpy, Window win, NL3D::IDriver *driver = NULL);
+
+	/**
+	 * sends all events to server
+	 * (should call CEventServer method postEvent() )
+	 */
+	virtual void submitEvents(CEventServer & server, bool allWindows);
+
+	/**
+	 * enable or disable mouse raw mode
+	 */
+	virtual void emulateMouseRawMode(bool emulate);
+
+	/**
+	 * process input-related events (mouse and keyboard)
+	 */
+	bool processMessage(XEvent &event, CEventServer *server = NULL);
+
+private:
+
+	// Private internal server message
+	class CUnixEventServer : CEventServer
+	{
+		friend class CUnixEventEmitter;
+	public:
+		void setServer (CEventServer *server)
+		{
+			_Server=server;
+		}
+	private:
+		virtual bool pumpEvent(CEvent* event)
+		{
+			CEventServer::pumpEvent(event);
+			_Server->postEvent (event);
+			return false;
+		}
+	private:
+		CEventServer *_Server;
+	};
+
+	void createIM();
+
+	Display*			_dpy;
+	Window				_win;
+	TKey				_PreviousKey;
+	XIM					_im;
+	XIC					_ic;
+	bool				_emulateRawMode;
+	NL3D::IDriver*		_driver;
+	CUnixEventServer	_InternalServer;
+};
+
+
+} // NLMISC
+
+#endif // defined(NL_OS_UNIX) && !defined(NL_OS_MAC)
+
+#endif // NL_UNIX_EVENT_EMITTER_H
+
+/* End of unix_event_emitter.h */
