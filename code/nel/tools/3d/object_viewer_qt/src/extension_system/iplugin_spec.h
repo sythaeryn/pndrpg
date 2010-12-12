e905d82b7419910f003a399de753e04ad88f0dd1
rti <rti@localhost>
2010-12-12 16:00:51 +0100

    Changed: #1153 app bundle plist references atys.icns

---
+++ code/nel/tools/3d/object_viewer_qt/src/extension_system/iplugin_spec.h
@@ -0,0 +1,82 @@
+/*
+    Object Viewer Qt
+    Copyright (C) 2010 Dzmitry Kamiahin <dnk-88@tut.by>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef IPLUGINSPEC_H
+#define IPLUGINSPEC_H
+
+#include <QtCore/QString>
+
+namespace NLQT
+{
+class IPlugin;
+class IPluginManager;
+
+/**
+@struct State
+@details The plugin goes through several steps while being loaded.
+The state gives a hint on what went wrong in case of an error.
+*/
+struct State
+{
+	enum List
+	{
+		Invalid = 1,
+		Read,
+		Loaded,
+		Initialized,
+		Running,
+		Stopped,
+		Deleted
+	};
+};
+
+/**
+@interface IPluginSpec
+@brief Interface for plugin spec contains the information of the plugins and 
+information about the plugin's current state.
+@details The plugin spec is also filled with more information as the plugin
+goes through its loading process (see State).
+If an error occurs, the plugin spec is the place to look for the error details.
+*/
+class IPluginSpec
+{
+public:
+	virtual ~IPluginSpec() {}
+
+	virtual QString name() const = 0;
+	virtual QString version() const = 0;
+	virtual QString vendor() const = 0;
+	virtual QString description() const = 0;
+
+	virtual QString location() const = 0;
+	virtual QString filePath() const = 0;
+	virtual QString fileName() const = 0;
+
+	virtual IPlugin *plugin() const = 0;
+
+	// state
+	virtual int getState() const = 0;
+	virtual bool hasError() const = 0;
+	virtual QString errorString() const = 0;
+};
+
+} // namespace NLQT
+
+#endif // IPLUGINSPEC_H
+
---
+++ code/nel/tools/3d/object_viewer_qt/src/plugins/example/plugin1.cpp
@@ -0,0 +1,99 @@
+#include "plugin1.h"
+
+#include <QtCore/QObject>
+#include <QtGui/QMessageBox>
+#include <QtGui/QMainWindow>
+#include <QtGui/QMenu>
+#include <QtGui/QAction>
+#include <QtGui/QMenuBar>
+
+#include "../../extension_system/iplugin_spec.h"
+
+#include "nel/misc/debug.h"
+
+using namespace Plugin;
+
+bool MyPlugin::initialize(NLQT::IPluginManager *pluginManager, QString *errorString)
+{
+	Q_UNUSED(errorString);
+	_plugMan = pluginManager;
+	QString str;
+	
+	QList<NLQT::IPluginSpec *> listPlug = pluginManager->plugins();
+	
+	Q_FOREACH (NLQT::IPluginSpec *plugSpec, listPlug)
+		str += plugSpec->name();
+
+	nlinfo(str.toStdString().c_str());
+
+	return true;
+}
+
+void MyPlugin::extensionsInitialized()
+{
+	QMenu *helpMenu = qobject_cast<QMenu *>(objectByName("ovqt.Menu.Help"));
+	nlassert(helpMenu);
+
+	helpMenu->addSeparator();
+	QAction *newAction = helpMenu->addAction("MyPlugin");
+	
+	connect(newAction, SIGNAL(triggered()), this, SLOT(execMessageBox()));
+}
+
+void MyPlugin::execMessageBox()
+{
+	QMainWindow *wnd = qobject_cast<QMainWindow *>(objectByName("CMainWindow"));
+	nlassert(wnd);
+	
+	QMessageBox msgBox;
+	msgBox.setText(wnd->objectName() + QString(": width=%1,height=%2").arg(wnd->width()).arg(wnd->height()));
+	msgBox.exec();
+}
+
+void MyPlugin::setNelContext(NLMISC::INelContext *nelContext)
+{
+#ifdef NL_OS_WINDOWS
+	// Ensure that a context doesn't exist yet.
+	// This only applies to platforms without PIC, e.g. Windows.
+	nlassert(!NLMISC::INelContext::isContextInitialised());
+#endif // NL_OS_WINDOWS
+	_LibContext = new NLMISC::CLibraryContext(*nelContext);
+}
+
+QString MyPlugin::name() const
+{
+	return "ExamplePlugin";
+}
+
+QString MyPlugin::version() const
+{
+	return "0.2";
+}
+
+QString MyPlugin::vendor() const
+{
+	return "dnk-88";
+}
+
+QString MyPlugin::description() const
+{
+	return "Example ovqt plugin.";
+}
+
+QObject* MyPlugin::objectByName(const QString &name) const
+{
+	Q_FOREACH (QObject *qobj, _plugMan->allObjects())
+		if (qobj->objectName() == name)
+				return qobj;
+	return 0;
+}
+
+NLQT::IPluginSpec *MyPlugin::pluginByName(const QString &name) const
+{
+	Q_FOREACH (NLQT::IPluginSpec *spec, _plugMan->plugins())
+		if (spec->name() == name)
+			return spec;
+	return 0;
+}
+
+Q_EXPORT_PLUGIN(MyPlugin)
