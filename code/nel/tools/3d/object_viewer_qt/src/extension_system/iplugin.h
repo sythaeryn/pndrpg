cd91d0e43a6e69c7a7d5fafc7eb2190cbe0e4ede
vl <vl@localhost>
2011-02-14 18:03:35 +0100

    Changed: disabled veget on mac since it crash

---
+++ code/nel/tools/3d/object_viewer_qt/src/extension_system/iplugin.h
@@ -0,0 +1,66 @@
+/*
+    Object Viewer Qt
+    Copyright (C) 2010 Dzmitry Kamiahin <dnk-88@tut.by>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef IPLUGIN_H
+#define IPLUGIN_H
+
+#include <QtCore/QtPlugin>
+#include <QtCore/QString>
+
+#include "iplugin_manager.h"
+
+namespace NLMISC
+{
+	class INelContext;
+}
+
+namespace ExtensionSystem
+{
+
+/**
+@interface IPlugin
+@brief Base class for all plugins.
+@details The IPlugin class is an abstract class that must be implemented 
+once for each plugin. The IPlugin implementation must be exported and 
+made known to Qt's plugin system via the Q_EXPORT_PLUGIN macro, 
+see the Qt documentation for details on that.
+*/
+class IPlugin
+{
+public:
+	virtual ~IPlugin() {}
+
+	virtual bool initialize(IPluginManager *pluginManager, QString *errorString) = 0;
+	virtual void extensionsInitialized() = 0;
+	virtual void shutdown() { }
+
+	virtual void setNelContext(NLMISC::INelContext *nelContext) = 0;
+
+	virtual QString name() const = 0;
+	virtual QString version() const = 0;
+	virtual QString vendor() const = 0;
+	virtual QString description() const = 0;
+	virtual QList<QString> dependencies() const = 0;
+};
+
+}; //namespace ExtensionSystem
+
+Q_DECLARE_INTERFACE(ExtensionSystem::IPlugin, "dev.ryzom.com.ObjectViewerQt.IPlugin/0.9.1")
+
+#endif // IPLUGIN_H
---
+++ code/nel/tools/3d/object_viewer_qt/src/extension_system/plugin_spec.h
@@ -0,0 +1,81 @@
+/*
+    Object Viewer Qt
+    Copyright (C) 2010 Dzmitry Kamiahin <dnk-88@tut.by>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef PLUGINSPEC_H
+#define PLUGINSPEC_H
+
+#include "iplugin_spec.h"
+
+namespace ExtensionSystem
+{
+
+class CPluginSpec: public IPluginSpec
+{
+public:
+	virtual QString name() const;
+	virtual QString version() const;
+	virtual QString vendor() const;
+	virtual QString description() const;
+
+	virtual QString location() const;
+	virtual QString filePath() const;
+	virtual QString fileName() const;
+
+	virtual IPlugin *plugin() const;
+
+	// state
+	virtual int getState() const;
+	virtual bool hasError() const;
+	virtual QString errorString() const;
+
+private:
+	CPluginSpec();
+
+	bool setFileName(const QString &fileName);
+	bool loadLibrary();
+	bool initializePlugin();
+	bool initializeExtensions();
+	void stop();
+	void kill();
+
+	bool reportError(const QString &err);
+
+	QString _location;
+	QString _filePath;
+	QString _fileName;
+
+	QString _name;
+	QString _version;
+	QString _vendor;
+	QString _description;
+
+	int _state;
+	bool _hasError;
+	QString _errorString;
+
+	IPlugin *_plugin;
+	IPluginManager *_pluginManager;
+
+	friend class CPluginManager;
+};
+
+} // namespace NLQT
+
+#endif // PLUGINSPEC_H
+
