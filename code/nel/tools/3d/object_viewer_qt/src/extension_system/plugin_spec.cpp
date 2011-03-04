1dab6835acf87b9637031529b868487e6f90d690
dnk-88 <dnk-88@localhost>
2011-03-04 23:56:37 +0200

    Changed: #1193 moved OV actions in a separatly menu.

---
+++ code/nel/tools/3d/object_viewer_qt/src/extension_system/plugin_spec.cpp
@@ -0,0 +1,269 @@
+// Object Viewer Qt - MMORPG Framework <http://dev.ryzom.com/projects/nel/>
+// Copyright (C) 2010  Winch Gate Property Limited
+// Copyright (C) 2011  Dzmitry Kamiahin <dnk-88@tut.by>
+// Parts by Nokia Corporation (qt-info@nokia.com) Copyright (C) 2009.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#include "plugin_spec.h"
+
+#include <QtCore/QList>
+#include <QtCore/QFile>
+#include <QtCore/QFileInfo>
+#include <QtCore/QPluginLoader>
+#include <QtCore/QCoreApplication>
+
+#include "nel/misc/app_context.h"
+
+#include "iplugin.h"
+#include "iplugin_manager.h"
+
+namespace ExtensionSystem
+{
+
+CPluginSpec::CPluginSpec()
+	: m_location(""),
+	  m_filePath(""),
+	  m_fileName(""),
+	  m_name(""),
+	  m_version(""),
+	  m_vendor(""),
+	  m_description(""),
+	  m_state(State::Invalid),
+	  m_hasError(false),
+	  m_errorString(""),
+	  m_plugin(0),
+	  m_pluginManager(0)
+{
+}
+
+QString CPluginSpec::name() const
+{
+	return m_name;
+}
+
+QString CPluginSpec::version() const
+{
+	return m_version;
+}
+
+QString CPluginSpec::vendor() const
+{
+	return m_vendor;
+}
+
+QString CPluginSpec::description() const
+{
+	return m_description;
+}
+
+QString CPluginSpec::location() const
+{
+	return m_location;
+}
+
+QString CPluginSpec::filePath() const
+{
+	return m_filePath;
+}
+
+QString CPluginSpec::fileName() const
+{
+	return m_fileName;
+}
+
+IPlugin* CPluginSpec::plugin() const
+{
+	return m_plugin;
+}
+
+int CPluginSpec::getState() const
+{
+	return m_state;
+}
+
+bool CPluginSpec::hasError() const
+{
+	return m_hasError;
+}
+
+QString CPluginSpec::errorString() const
+{
+	return m_errorString;
+}
+
+QList<CPluginSpec *> CPluginSpec::dependencySpecs() const
+{
+	return m_dependencySpecs;
+}
+
+bool CPluginSpec::setFileName(const QString &fileName)
+{
+	QFile file(fileName);
+	if (!file.exists())
+		return reportError(QCoreApplication::translate("CPluginSpec", "File does not exist: %1").arg(file.fileName()));
+	if (!file.open(QIODevice::ReadOnly))
+		return reportError(QCoreApplication::translate("CPluginSpec", "Could not open file for read: %1").arg(file.fileName()));
+
+	QFileInfo fileInfo(file);
+	m_location = fileInfo.absolutePath();
+	m_filePath = fileInfo.absoluteFilePath();
+	m_fileName = fileInfo.fileName();
+
+	m_state = State::Read;
+	return true;
+}
+
+bool CPluginSpec::loadLibrary()
+{
+	if (m_hasError)
+		return false;
+	if (m_state != State::Read)
+	{
+		if (m_state == State::Loaded)
+			return true;
+		return reportError(QCoreApplication::translate("CPluginSpec", "Loading the library failed because state != Resolved"));
+	}
+
+	QPluginLoader loader(m_filePath);
+	if (!loader.load())
+		return reportError(loader.errorString());
+
+	IPlugin *pluginObject = qobject_cast<IPlugin *>(loader.instance());
+	if (!pluginObject)
+	{
+		loader.unload();
+		return reportError(QCoreApplication::translate("CPluginSpec", "Plugin is not valid (does not derive from IPlugin)"));
+	}
+
+	pluginObject->setNelContext(&NLMISC::INelContext::getInstance());
+
+	m_name = pluginObject->name();
+	m_version = pluginObject->version();
+	m_vendor = pluginObject->vendor();
+	m_description = pluginObject->description();
+
+	m_state = State::Loaded;
+	m_plugin = pluginObject;
+	return true;
+}
+
+bool CPluginSpec::resolveDependencies(const QList<CPluginSpec *> &specs)
+{
+	if (m_hasError)
+		return false;
+	if (m_state != State::Loaded)
+	{
+		m_errorString = QCoreApplication::translate("CPluginSpec", "Resolving dependencies failed because state != Read");
+		m_hasError = true;
+		return false;
+	}
+	QList<CPluginSpec *> resolvedDependencies;
+	QStringList dependencies = m_plugin->dependencies();
+	Q_FOREACH(const QString &dependency, dependencies)
+	{
+		CPluginSpec *found = 0;
+
+		Q_FOREACH(CPluginSpec *spec, specs)
+		{
+			if (QString::compare(dependency, spec->name(), Qt::CaseInsensitive) == 0)
+			{
+				found = spec;
+				break;
+			}
+		}
+		if (!found)
+		{
+			m_hasError = true;
+			if (!m_errorString.isEmpty())
+				m_errorString.append(QLatin1Char('\n'));
+			m_errorString.append(QCoreApplication::translate("CPluginSpec", "Could not resolve dependency '%1'")
+								 .arg(dependency));
+			continue;
+		}
+		resolvedDependencies.append(found);
+	}
+	if (m_hasError)
+		return false;
+
+	m_dependencySpecs = resolvedDependencies;
+
+	m_state = State::Resolved;
+
+	return true;
+}
+
+bool CPluginSpec::initializePlugin()
+{
+	if (m_hasError)
+		return false;
+	if (m_state != State::Resolved)
+	{
+		if (m_state == State::Initialized)
+			return true;
+		return reportError(QCoreApplication::translate("CPluginSpec", "Initializing the plugin failed because state != Resolved)"));
+	}
+	if (!m_plugin)
+		return reportError(QCoreApplication::translate("CPluginSpec", "Internal error: have no plugin instance to initialize"));
+
+	QString err;
+	if (!m_plugin->initialize(m_pluginManager, &err))
+		return reportError(QCoreApplication::translate("CPluginSpec", "Plugin initialization failed: %1").arg(err));
+
+	m_state = State::Initialized;
+	return true;
+}
+
+bool CPluginSpec::initializeExtensions()
+{
+	if (m_hasError)
+		return false;
+	if (m_state != State::Initialized)
+	{
+		if (m_state == State::Running)
+			return true;
+		return reportError(QCoreApplication::translate("CPluginSpec", "Cannot perform extensionsInitialized because state != Initialized"));
+	}
+	if (!m_plugin)
+		return reportError(QCoreApplication::translate("CPluginSpec", "Internal error: have no plugin instance to perform extensionsInitialized"));
+
+	m_plugin->extensionsInitialized();
+	m_state = State::Running;
+	return true;
+}
+
+void CPluginSpec::stop()
+{
+	if (!m_plugin)
+		return;
+	m_plugin->shutdown();
+	m_state = State::Stopped;
+}
+
+void CPluginSpec::kill()
+{
+	if (!m_plugin)
+		return;
+	delete m_plugin;
+	m_plugin = 0;
+	m_state = State::Deleted;
+}
+
+bool CPluginSpec::reportError(const QString &err)
+{
+	m_errorString = err;
+	m_hasError = true;
+	return false;
+}
+
+} // namespace NLQT
---
+++ code/nel/tools/3d/object_viewer_qt/src/plugins/core/main_window.cpp
@@ -0,0 +1,278 @@
+// Object Viewer Qt - MMORPG Framework <http://dev.ryzom.com/projects/nel/>
+// Copyright (C) 2010  Winch Gate Property Limited
+// Copyright (C) 2011  Dzmitry Kamiahin <dnk-88@tut.by>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// Project includes
+#include "main_window.h"
+#include "icontext.h"
+#include "icore_listener.h"
+#include "menu_manager.h"
+#include "core.h"
+#include "core_constants.h"
+#include "settings_dialog.h"
+
+// NeL includes
+#include <nel/misc/debug.h>
+
+// Qt includes
+#include <QtCore/QCoreApplication>
+#include <QtGui/QtGui>
+
+namespace Core
+{
+
+MainWindow::MainWindow(ExtensionSystem::IPluginManager *pluginManager, QWidget *parent)
+	: QMainWindow(parent),
+	  m_pluginManager(0),
+	  m_menuManager(0),
+	  m_coreImpl(0),
+	  m_lastDir("."),
+	  m_settings(0)
+{
+	QCoreApplication::setApplicationName(QLatin1String("ObjectViewerQt"));
+	QCoreApplication::setApplicationVersion(QLatin1String(Core::Constants::OVQT_VERSION_LONG));
+	QCoreApplication::setOrganizationName(QLatin1String("RyzomCore"));
+
+	setObjectName(Constants::MAIN_WINDOW);
+	setWindowIcon(QIcon(Constants::ICON_NEL));
+	setWindowTitle(tr("Object Viewer Qt"));
+
+	m_pluginManager = pluginManager;
+	m_settings = m_pluginManager->settings();
+	m_coreImpl = new CoreImpl(this);
+
+	m_menuManager = new MenuManager(this);
+	m_menuManager->setMenuBar(menuBar());
+
+	m_tabWidget = new QTabWidget(this);
+	m_tabWidget->setTabPosition(QTabWidget::South);
+	m_tabWidget->setMovable(true);
+	setCentralWidget(m_tabWidget);
+
+	setDockNestingEnabled(true);
+	m_originalPalette = QApplication::palette();
+
+	createDialogs();
+	createActions();
+	createMenus();
+	createStatusBar();
+}
+
+MainWindow::~MainWindow()
+{
+	m_pluginManager->removeObject(m_coreImpl);
+	m_pluginManager->removeObject(m_menuManager);
+
+	delete m_coreImpl;
+	m_coreImpl = 0;
+}
+
+bool MainWindow::initialize(QString *errorString)
+{
+	Q_UNUSED(errorString);
+	m_pluginManager->addObject(m_coreImpl);
+	m_pluginManager->addObject(m_menuManager);
+	return true;
+}
+
+void MainWindow::extensionsInitialized()
+{
+	QList<IContext *> listContexts = m_pluginManager->getObjects<IContext>();
+
+	Q_FOREACH(IContext *context, listContexts)
+	{
+		addContextObject(context);
+	}
+
+	connect(m_pluginManager, SIGNAL(objectAdded(QObject *)), this, SLOT(checkObject(QObject *)));
+	readSettings();
+	show();
+}
+
+IMenuManager *MainWindow::menuManager() const
+{
+	return m_menuManager;
+}
+
+QSettings *MainWindow::settings() const
+{
+	return m_settings;
+}
+
+ExtensionSystem::IPluginManager *MainWindow::pluginManager() const
+{
+	return m_pluginManager;
+}
+
+void MainWindow::checkObject(QObject *obj)
+{
+	IContext *context = qobject_cast<IContext *>(obj);
+	if (context)
+		addContextObject(context);
+}
+
+bool MainWindow::showOptionsDialog(const QString &group,
+								   const QString &page,
+								   QWidget *parent)
+{
+	if (!parent)
+		parent = this;
+	CSettingsDialog settingsDialog(m_pluginManager, group, page, parent);
+	settingsDialog.show();
+	return settingsDialog.execDialog();
+}
+
+void MainWindow::about()
+{
+	QMessageBox::about(this, tr("About Object Viewer Qt"),
+					   tr("<h2>Object Viewer Qt NG</h2>"
+						  "<p> Author: dnk-88 <p>Compiled on %1 %2").arg(__DATE__).arg(__TIME__));
+}
+
+void MainWindow::closeEvent(QCloseEvent *event)
+{
+	QList<ICoreListener *> listeners = m_pluginManager->getObjects<ICoreListener>();
+	Q_FOREACH(ICoreListener *listener, listeners)
+	{
+		if (!listener->closeMainWindow())
+		{
+			event->ignore();
+			return;
+		}
+	}
+	Q_EMIT m_coreImpl->closeMainWindow();
+
+	writeSettings();
+	event->accept();
+}
+
+void MainWindow::addContextObject(IContext *context)
+{
+	QWidget *tabWidget = new QWidget(m_tabWidget);
+	m_tabWidget->addTab(tabWidget, context->icon(), context->trName());
+	QGridLayout *gridLayout = new QGridLayout(tabWidget);
+	gridLayout->setObjectName(QString::fromUtf8("gridLayout_") + context->id());
+	gridLayout->setContentsMargins(0, 0, 0, 0);
+	gridLayout->addWidget(context->widget(), 0, 0, 1, 1);
+}
+
+void MainWindow::createActions()
+{
+	m_openAction = new QAction(tr("&Open..."), this);
+	m_openAction->setIcon(QIcon(":/images/open-file.png"));
+	m_openAction->setShortcut(QKeySequence::Open);
+	m_openAction->setStatusTip(tr("Open an existing file"));
+	menuManager()->registerAction(m_openAction, Constants::OPEN);
+//	connect(m_openAction, SIGNAL(triggered()), this, SLOT(open()));
+
+	m_exitAction = new QAction(tr("E&xit"), this);
+	m_exitAction->setShortcut(QKeySequence(tr("Ctrl+Q")));
+	m_exitAction->setStatusTip(tr("Exit the application"));
+	menuManager()->registerAction(m_exitAction, Constants::EXIT);
+	connect(m_exitAction, SIGNAL(triggered()), this, SLOT(close()));
+
+	m_settingsAction = new QAction(tr("&Settings"), this);
+	m_settingsAction->setIcon(QIcon(":/images/preferences.png"));
+	m_settingsAction->setShortcut(QKeySequence::Preferences);
+	m_settingsAction->setStatusTip(tr("Open the settings dialog"));
+	menuManager()->registerAction(m_settingsAction, Constants::SETTINGS);
+	connect(m_settingsAction, SIGNAL(triggered()), this, SLOT(showOptionsDialog()));
+
+	m_aboutAction = new QAction(tr("&About"), this);
+	m_aboutAction->setStatusTip(tr("Show the application's About box"));
+	menuManager()->registerAction(m_aboutAction, Constants::ABOUT);
+	connect(m_aboutAction, SIGNAL(triggered()), this, SLOT(about()));
+
+	m_aboutQtAction = new QAction(tr("About &Qt"), this);
+	m_aboutQtAction->setStatusTip(tr("Show the Qt library's About box"));
+	menuManager()->registerAction(m_aboutQtAction, Constants::ABOUT_QT);
+	connect(m_aboutQtAction, SIGNAL(triggered()), qApp, SLOT(aboutQt()));
+
+	m_pluginViewAction = new QAction(tr("About &Plugins"), this);
+	m_pluginViewAction->setStatusTip(tr("Show the plugin view dialog"));
+	menuManager()->registerAction(m_pluginViewAction, Constants::ABOUT_PLUGINS);
+	connect(m_pluginViewAction, SIGNAL(triggered()), m_pluginView, SLOT(show()));
+
+#ifdef Q_WS_MAC
+	m_exitAction->setMenuRole(QAction::QuitRole);
+	m_settingsAction->setMenuRole(QAction::PreferencesRole);
+	m_aboutAction->setMenuRole(QAction::AboutRole);
+	m_aboutQtAction->setMenuRole(QAction::AboutQtRole);
+	m_pluginViewAction->setMenuRole(QAction::ApplicationSpecificRole);
+#endif
+}
+
+void MainWindow::createMenus()
+{
+	m_fileMenu = menuBar()->addMenu(tr("&File"));
+	menuManager()->registerMenu(m_fileMenu, Constants::M_FILE);
+	m_fileMenu->addSeparator();
+	m_fileMenu->addAction(m_exitAction);
+
+	m_editMenu = menuBar()->addMenu(tr("&Edit"));
+	menuManager()->registerMenu(m_editMenu, Constants::M_EDIT);
+
+	m_viewMenu = menuBar()->addMenu(tr("&View"));
+	menuManager()->registerMenu(m_viewMenu, Constants::M_VIEW);
+
+	m_toolsMenu = menuBar()->addMenu(tr("&Tools"));
+	menuManager()->registerMenu(m_toolsMenu, Constants::M_TOOLS);
+
+	m_sheetMenu = m_toolsMenu->addMenu(tr("&Sheet"));
+	menuManager()->registerMenu(m_sheetMenu, Constants::M_SHEET);
+
+//	m_toolsMenu->addSeparator();
+
+	m_toolsMenu->addAction(m_settingsAction);
+
+	menuBar()->addSeparator();
+
+	m_helpMenu = menuBar()->addMenu(tr("&Help"));
+	menuManager()->registerMenu(m_helpMenu, Constants::M_HELP);
+	m_helpMenu->addAction(m_aboutAction);
+	m_helpMenu->addAction(m_aboutQtAction);
+	m_helpMenu->addAction(m_pluginViewAction);
+}
+
+void MainWindow::createStatusBar()
+{
+	statusBar()->showMessage(tr("StatusReady"));
+}
+
+void MainWindow::createDialogs()
+{
+	m_pluginView = new ExtensionSystem::CPluginView(m_pluginManager, this);
+}
+
+void MainWindow::readSettings()
+{
+	m_settings->beginGroup("MainWindow");
+	restoreState(m_settings->value("WindowState").toByteArray());
+	restoreGeometry(m_settings->value("WindowGeometry").toByteArray());
+	m_settings->endGroup();
+}
+
+void MainWindow::writeSettings()
+{
+	m_settings->beginGroup("MainWindow");
+	m_settings->setValue("WindowState", saveState());
+	m_settings->setValue("WindowGeometry", saveGeometry());
+	m_settings->endGroup();
+}
+
+} /* namespace Core */
+
+/* end of file */
