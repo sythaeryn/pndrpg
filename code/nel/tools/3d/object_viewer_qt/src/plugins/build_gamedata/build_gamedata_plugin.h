30d1733130117280de1b7eb7781bd7147c978601
sfb <sfb@localhost>
2011-09-12 16:14:47 -0500

    Changed: Initial build_gamedata plugin for OVQT, UI layouts and basic API.

---
+++ code/nel/tools/3d/object_viewer_qt/src/plugins/build_gamedata/build_gamedata_plugin.h
@@ -0,0 +1,78 @@
+// Object Viewer Qt - MMORPG Framework <http://dev.ryzom.com/projects/nel/>
+// Copyright (C) 2010  Winch Gate Property Limited
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef BUILD_GAMEDATA_PLUGIN_H
+#define BUILD_GAMEDATA_PLUGIN_H
+
+#include "../../extension_system/iplugin.h"
+
+#include "nel/misc/app_context.h"
+
+#include <QtCore/QObject>
+
+namespace NLMISC
+{
+class CLibraryContext;
+}
+
+namespace NLQT
+{
+class IPluginSpec;
+}
+
+class BuildGamedataView;
+
+namespace BuildGamedata
+{
+
+class CBuildGamedataSettingsPage;
+
+class BuildGamedataPlugin : public QObject, public ExtensionSystem::IPlugin
+{
+	Q_OBJECT
+	Q_INTERFACES(ExtensionSystem::IPlugin)
+public:
+	BuildGamedataPlugin(QWidget *parent = 0);
+	~BuildGamedataPlugin();
+
+
+	bool initialize(ExtensionSystem::IPluginManager *pluginManager, QString *errorString);
+	void extensionsInitialized();
+
+	void setNelContext(NLMISC::INelContext *nelContext);
+
+	void addAutoReleasedObject(QObject *obj);
+
+	QString name() const;
+	QString version() const;
+	QString vendor() const;
+	QString description() const;
+	QStringList dependencies() const;
+
+protected:
+	NLMISC::CLibraryContext *_LibContext;
+
+private:
+	ExtensionSystem::IPluginManager *m_plugMan;
+	BuildGamedataView *m_buildGamedataView;
+	CBuildGamedataSettingsPage *m_buildGamedataSettingsPage;
+	QList<QObject *> m_autoReleaseObjects;
+
+};
+
+} // namespace BuildGamedata
+
+#endif // BUILD_GAMEDATA_PLUGIN_H
