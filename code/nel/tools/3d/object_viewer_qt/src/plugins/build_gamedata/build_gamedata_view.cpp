a5ac39e8d810790f15fd3178b125e017604287f7
dnk-88 <dnk-88@localhost>
2011-09-14 11:34:45 +0300

    Changed: Cleared ui layout.

---
+++ code/nel/tools/3d/object_viewer_qt/src/plugins/build_gamedata/build_gamedata_view.cpp
@@ -0,0 +1,40 @@
+// Object Viewer Qt - MMORPG Framework <http://dev.ryzom.com/projects/nel/>
+// Copyright (C) 2010  Winch Gate Property Limited
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "build_gamedata_view.h"
+
+#include "nel/misc/path.h"
+
+#include <QtGui/QMessageBox>
+#include <QtGui/QApplication>
+
+class	CPred
+{
+public:
+	bool	operator()(const NLMISC::CSheetId &a, const NLMISC::CSheetId &b)
+	{
+		return a.toString()<b.toString();
+	}
+};
+
+BuildGamedataView::BuildGamedataView(QWidget *parent) : QDockWidget(parent)
+{
+	m_ui.setupUi(this);
+}
+
+BuildGamedataView::~BuildGamedataView()
+{
+}
