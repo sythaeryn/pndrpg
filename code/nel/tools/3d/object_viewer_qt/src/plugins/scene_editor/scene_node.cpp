abd8b74b0696a05521279702028c4b800229043f
dnk-88 <dnk-88@localhost>
2012-04-24 21:36:43 +0300

    Changed: #1375 Updated scene nodes

--- code/nel/tools/3d/object_viewer_qt/src/plugins/scene_editor/scene_node.cpp
+++ code/nel/tools/3d/object_viewer_qt/src/plugins/scene_editor/scene_node.cpp
@@ -16,6 +16,8 @@
 
 #include "scene_node.h"
 
+#include "scene_editor_constants.h"
+
 namespace SceneEditor
 {
 
@@ -29,9 +31,7 @@ SceneNode::~SceneNode()
 	if (m_parent)
 		m_parent->removeChildNode(this);
 
-	qDeleteAll(m_children);
-	nlassert(m_children.isEmpty());
-	m_data.clear();
+	deleteAllChilds();
 }
 
 void SceneNode::prependChildNode(SceneNode *node)
@@ -122,6 +122,12 @@ int SceneNode::childCount() const
 	return m_children.count();
 }
 
+void SceneNode::deleteAllChilds()
+{
+	qDeleteAll(m_children);
+	m_children.clear();
+}
+
 QVariant SceneNode::data(int key) const
 {
 	return m_data[key];
@@ -148,6 +154,186 @@ int SceneNode::row() const
 SceneNode::NodeType SceneNode::type() const
 {
 	return BasicNodeType;
+}
+
+/// Transform scene node
+TransformNode::TransformNode(NL3D::UScene *scene)
+	: SceneNode(),
+	m_scene(scene)
+{
+	nlassert(m_scene);
+	m_transform = m_scene->createTransform();
+}
+
+TransformNode::TransformNode(NL3D::UTransform transform)
+	: m_scene(0)
+{
+	m_transform = transform;
+}
+
+TransformNode::TransformNode()
+	: m_scene(0)
+{
+}
+
+TransformNode::~TransformNode()
+{
+	if (m_scene != 0)
+		m_scene->deleteTransform(m_transform);
+}
+
+SceneNode::NodeType TransformNode::type() const
+{
+	return TransformNodeType;
+}
+
+void TransformNode::bind(NL3D::UTransform transform)
+{
+	transform.parent(transform);
+}
+
+void TransformNode::setTransform(NL3D::UTransform transform)
+{
+	m_transform = transform;
+}
+
+NL3D::UTransform TransformNode::transform() const
+{
+	return m_transform;
+}
+
+/// Instance scene node
+InstanceNode::InstanceNode(NL3D::UScene *scene, const QString &fileName)
+	: TransformNode(),
+	m_scene(scene)
+{
+	nlassert(m_scene);
+	m_instance = m_scene->createInstance(fileName.toStdString());
+	if  (!m_instance.empty())
+	{
+		setTransform(m_instance);
+		m_instance.setTransformMode(NL3D::UTransformable::RotEuler);
+		m_instance.start();
+	}
+	else
+	{
+		nlerror("createInstance %s returned 0 -> no shape.", fileName.toStdString().c_str());
+	}
+	setData(Constants::NODE_FILE_NAME, fileName);
+}
+
+InstanceNode::InstanceNode()
+	: TransformNode(),
+	m_scene(0)
+{
+}
+
+InstanceNode::InstanceNode(NL3D::UInstance instance)
+	: TransformNode(),
+	m_scene(0)
+{
+	m_instance = instance;
+	setTransform(m_instance);
+}
+
+InstanceNode::~InstanceNode()
+{
+	if ((m_scene != 0) && (!m_instance.empty()))
+		m_scene->deleteInstance(m_instance);
+}
+
+SceneNode::NodeType InstanceNode::type() const
+{
+	return InstanceNodeType;
+}
+
+void InstanceNode::setInstance(NL3D::UInstance instance)
+{
+	m_instance = instance;
+	setTransform(instance);
+}
+
+NL3D::UInstance InstanceNode::instance() const
+{
+	return m_instance;
+}
+
+/// Skeleton scene node
+SkeletonNode::SkeletonNode(NL3D::UScene *scene, const QString &fileName)
+	: TransformNode(),
+	m_scene(0)
+{
+	nlassert(m_scene);
+	m_skeleton = m_scene->createSkeleton(fileName.toStdString());
+	m_skelTransform = m_scene->createTransform();
+
+	if (!m_skeleton.empty())
+	{
+		m_skeleton.parent(m_skelTransform);
+	}
+	else
+		nlerror("createSkeleton %s returned 0 -> no skeleton.", fileName.toStdString().c_str());
+
+	setData(Constants::NODE_FILE_NAME, fileName);
+}
+
+SkeletonNode::~SkeletonNode()
+{
+	if (!m_skeleton.empty())
+	{
+		m_scene->deleteSkeleton( m_skeleton );
+		m_scene->deleteTransform(m_skelTransform);
+	}
+}
+
+SceneNode::NodeType SkeletonNode::type() const
+{
+	return SkeletonNodeType;
+}
+
+NL3D::USkeleton SkeletonNode::skeleton() const
+{
+	return m_skeleton;
+}
+
+void SkeletonNode::bind(NL3D::UTransform transform)
+{
+	NL3D::UInstance instance;
+	instance.cast(transform);
+
+	// TODO: strong check
+	m_skeleton.bindSkin(instance);
+}
+
+/// Particle system scene node
+ParticleSystemNode::ParticleSystemNode(NL3D::UScene *scene, const QString &fileName)
+	: InstanceNode(),
+	m_scene(scene)
+{
+	nlassert(m_scene);
+
+	NL3D::UInstance instance = m_scene->createInstance(fileName.toStdString());
+	if  (!instance.empty())
+	{
+		setInstance(instance);
+		m_particleSystem.cast(instance);
+	}
+	else
+	{
+		nlerror("createInstance %s returned 0 -> no particle system.", fileName.toStdString().c_str());
+	}
+	setData(Constants::NODE_FILE_NAME, fileName);
+}
+
+ParticleSystemNode::~ParticleSystemNode()
+{
+	if (!m_particleSystem.empty())
+		m_scene->deleteInstance(m_particleSystem);
+}
+
+SceneNode::NodeType ParticleSystemNode::type() const
+{
+	return ParticleSystemNodeType;
 }
 
 } /* namespace SceneEditor */
