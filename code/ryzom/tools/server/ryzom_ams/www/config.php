c2598c967344ba2a7b0ff7abdd19639a1db4ff4e
Botanic <Botanic@localhost>
2013-06-15 07:08:38 -0700

    changed to use .php not html

---
+++ code/ryzom/tools/server/ryzom_ams/www/config.php
@@ -0,0 +1,26 @@
+<?php
+
+// This file contains all variables needed by other php scripts
+// ----------------------------------------------------------------------------------------
+// Variables for database access
+// ----------------------------------------------------------------------------------------
+// where we can find the mysql database
+$DBHOST = 'localhost' ;
+ $DBNAME = 'nel' ;
+ $DBUSERNAME = 'shard' ;
+ $DBPASSWORD = '' ;
+
+// If true= the server will add automatically unknown user in the database
+// (in nel.user= nel.permission= ring.ring_user and ring.characters
+$ALLOW_UNKNOWN = true ;
+// if true= the login service automaticaly create a ring user and a editor character if needed
+$CREATE_RING = true ;
+
+ // site paths definitions
+$AMS_LIB = dirname( dirname( __FILE__ ) ) . '/ams_lib';
+$AMS_TRANS = $AMS_LIB . '/translations';
+$AMS_CACHEDIR = $AMS_LIB . '/cache';
+
+$DEFAULT_LANGUAGE = 'en';
+
+$SITEBASE = dirname( __FILE__ ) . '/html/' ;
